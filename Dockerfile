f99f77ef2aec3551a948e535447c67d978b10ffa
Guilherme Macedo <guilherme@gmacedo.com>
2022-06-13 08:46:22 +0200

    Move build image to SLE BCI (#7)
    Signed-off-by: Guilherme Macedo <guilherme.macedo@suse.com>

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
-ARG ALPINE=alpine:latest
-FROM ${ALPINE} AS alpine
+FROM registry.suse.com/bci/bci-base:15.3.17.17.8 AS build
+
 ARG ARCH
 ARG KUBERNETES_RELEASE=v1.21.3
 WORKDIR /bin
 RUN set -x \
- && apk --no-cache add curl \
+ && zypper -n install curl \
  && curl -fsSLO https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_RELEASE}/bin/linux/${ARCH}/kubectl \
  && chmod +x kubectl
+RUN useradd -u 1000 -U -m kubectl
 
 FROM scratch
-COPY --from=alpine /bin/kubectl /bin/kubectl
+COPY --from=build /bin/kubectl /bin/kubectl
+COPY --from=build /etc/passwd /etc/passwd
+COPY --from=build /etc/group /etc/group
+USER kubectl
 ENTRYPOINT ["/bin/kubectl"]
 CMD ["help"]
--- multiarch-build-and-push.sh
+++ multiarch-build-and-push.sh
@@ -8,12 +8,11 @@ docker_image_build_and_push()
 {
   docker buildx build \
     --build-arg ARCH=${1?required} \
-    --build-arg ALPINE=${2?required} \
-    --build-arg KUBERNETES_RELEASE=${3?required} \
+    --build-arg KUBERNETES_RELEASE=${2?required} \
     --platform linux/${1} \
-    --tag ${4?required}:${3}-${1} \
-  ${5?required}
-  docker image push ${4?required}:${3}-${1}
+    --tag ${3?required}:${2}-${1} \
+  ${4?required}
+  docker image push ${3?required}:${2}-${1}
 }
 
 docker_manifest_create_and_push()
@@ -26,9 +25,8 @@ docker_manifest_create_and_push()
   docker manifest push $1
 }
 
-
-docker_image_build_and_push amd64  amd64/alpine:latest   ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
-docker_image_build_and_push arm64  arm64v8/alpine:latest ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
-docker_image_build_and_push arm    arm32v7/alpine:latest ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
+docker_image_build_and_push amd64 ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
+docker_image_build_and_push arm64 ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
+docker_image_build_and_push s390x ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
 
 docker_manifest_create_and_push ${REPO}:${KUBERNETES_RELEASE}
